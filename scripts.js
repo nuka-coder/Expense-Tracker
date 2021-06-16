ca5bc33c233a358a9751e72e92c33cfd88abfc78
houdinibeanie <jaybritany@gmail.com>
2021-06-16 00:46:20 -0400

    change to scripts/html sending inputs to td elements !colspan columns.

--- scripts.js
+++ scripts.js
@@ -6,65 +6,85 @@ form.addEventListener("submit", submitPushed);
 
 //declare function for submitPushed
 function submitPushed(event) {
-  //When name input value is submitted create new td node with Name value and append it to
 
-//declare new Row to hold 3 td inputs
+//Goal: When name input value is submitted create new td node with input value and append it to a table row...
 
-
-  //For Name Input
-  //declare variable for td cellName for all Name entries
-  var nameNode = document.createElement("TD");
-  //declare variable for Name input
-  var nameValue = document.getElementById("name");
-  //set innerHTML of nameNode to input
-  nameNode.innerHTML= nameValue.value;
-  //append nameNode element to tableRow
-  var row1 = document.getElementById("tableRow").appendChild(nameNode);
-//set class attribute
-nameNode.setAttribute("class", "nameAtt");
+                                      //For Name Input
+  //declare variable for new table row that will house all submitted inputs in one row
+  var newRow = document.createElement("TR");
+  //declare variable for tableBody within HTML page that the table row will append to
+  var tableBody = document.getElementById("tableBody");
+  //append newRow to tableBody
+  tableBody.appendChild(newRow);
+  //declare variable for td element to place name input value to
+  var nameTd = document.createElement("TD");
+  //declare variable for the name input
+  var nameInput = document.getElementById("name");
+  //assign name td element name input value
+  nameTd.innerHTML= nameInput.value;
+  //append td to newRow
+  newRow.appendChild(nameTd);
+  //assign class and class value to newRow
+  newRow.setAttribute("class", "rowData");
 
   //clear input text box
-  nameValue.value="";
+  nameInput.value="";
 
-  //For Date Input
-  //declare variable for td cellDate for all Date entries
-  var dateNode = document.getElementById("cellDate");
-  //declare variable for Date input
-  var dateValue = document.getElementById("date");
-  //set innerHTML of dateNode to input
-  dateNode.innerHTML+= dateValue.value + "<br>";
-  //clear input text box
-  dateValue.value="";
+                                            //For Date Input
+  //declare variable for td element to place date input value to
+  var dateTd = document.createElement("TD");
+  //declare variable for the date input
+  var dateInput = document.getElementById("date");
+  //assign date td element date input value
+  dateTd.innerHTML= dateInput.value;
+  //append td to newRow
+  newRow.appendChild(dateTd);
 
-  //For Number Input
-  //declare variable for td cellAmount for all Amount entries
-  var amountNode = document.getElementById("cellAmount");
-  //declare variable for Date input
-  var amountValue = document.getElementById("amount");
-  //set innerHTML of dateNode to input
-  amountNode.innerHTML+= "$" + amountValue.value + "<br>";
   //clear input text box
-  amountValue.value="";
+  dateInput.value="";
+
+                                        //For Amount Input
+  //declare variable for td element to place amount input value to
+  var amountTd = document.createElement("TD");
+  //declare variable for the amount input
+  var amountInput = document.getElementById("amount");
+  //assign amount td element amount input value
+  amountTd.innerHTML= amountInput.value;
+  //append td to newRow
+  newRow.appendChild(amountTd);
+
+  //clear input text box
+  amountInput.value="";
+
 
 //create button Element
 var deleteBtn = document.createElement("BUTTON");
 //set X for value of Button
 deleteBtn.innerHTML="X";
-//declare variable for td delete element
-var deleteNode = document.getElementById("delete");
-//append deleteBtn to deleteNode
-deleteNode.appendChild(deleteBtn);
-//createElement for <br>
-var br = document.createElement("BR");
-//append <br> to deleteNode
-deleteNode.appendChild(br);
-//adjust styling of delete th so that it appears
-deleteNode.style.backgroundColor="grey";
+//append deleteBtn to newRow
+newRow.appendChild(deleteBtn);
+//assign class and class value to deleteBtn
+deleteBtn.setAttribute("class", "btnDel");
 document.querySelector(".deleteHeading").style.border="1px solid black";
 
+//Goal: delete row content onclick of deleteBtn...
 
+//addEventListener to delete button
+deleteBtn.addEventListener("click", deleteRow);
+//declare function for deleteRow to delete a row
+function deleteRow() {
+  document.getElementById("table").deleteRow(1);
+}
+console.log(tableBody);
 
+//prevent default refresh of page on submit
   event.preventDefault();
 }
 
-//declaring
+// //delete row content onclick of deleteBtn
+// //addEventListener to delete button
+// deleteBtn.addEventListener("click", deleteRow);
+// //declare function deleteRow
+// function deleteRow() {
+//   document.getElementById("table").deleteRow(1);
+// }
